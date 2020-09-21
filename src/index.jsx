da1229f6b1dfe72ecc73d71f28c4ece31b36c2ea
Laurent Treuil <laurent.treuil@gmail.com>
2020-09-21 12:09:37 +0200

    state tree, store, middlewares

--- src/index.jsx
+++ src/index.jsx
@@ -9,18 +9,29 @@ import { createHistory as history } from 'history';
 
 import '../assets/stylesheets/application.scss';
 
+const garageName = prompt("What's your garage") || `garage${Math.floor(10 + (Math.random() * 90))}`;
+const initialState = {
+  garage: garageName,
+  cars: []
+};
+
+
 const reducers = combineReducers({
-  // key: reducer
+  garage: (state = null, action) => state,
+  cars: carsReducer,
+
 });
 
 const middlewares = applyMiddleware(reduxPromise, logger);
 
+const store = { reducers, initialState, middlewares };
+
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={createStore(store)}>
     <Router history={history}>
       <Switch>
-        TODO
+        <Route path="/" exact component={CarIndex} />
       </Switch>
     </Router>
   </Provider>,
