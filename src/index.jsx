90ef58c131bc8ca91eab53dc64fd6e7b84184c5b
Laurent Treuil <laurent.treuil@gmail.com>
2020-09-21 12:13:54 +0200

    car array before callin API

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,13 @@ import '../assets/stylesheets/application.scss';
 const garageName = prompt("What's your garage") || `garage${Math.floor(10 + (Math.random() * 90))}`;
 const initialState = {
   garage: garageName,
-  cars: []
+  cars:
+    [
+      { id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
+      { id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
+      { id: 3, brand: 'Aston Martin', model: 'DB Mark III', owner: 'James', plate: '418-ED-94' },
+      { id: 4, brand: 'VW', model: 'Beetle', owner: 'George', plate: '1234-XD-75' }
+    ]
 };
 
 
